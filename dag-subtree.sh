e543c0adfdeab9a0b50710c3f30f033eb5221fbe
Frank Bardon Jr <bardonf@upenn.edu>
2015-08-05 08:39:06 -0400

    [Subsplit] Now allows --components --bridges and --bundles options so we can limit our export time.

--- dag-subtree.sh
+++ dag-subtree.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
 # Ensure git subsplit is installed.
-if [ ! type "git subsplit" > /dev/null ]; then
+if [ ! type "git subsplit" >/dev/null 2>&1 ]; then
 
     echo "To run this command, you must first install git subsplit."
     echo "Visit: https://github.com/dflydev/git-subsplit"
     exit 1
 
 fi
+
+COMPONENTS=false
+BUNDLES=false
+FULL=false
+
+# Parse incoming arguments
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+    --components)
+    COMPONENTS=true
+    shift
+    ;;
+    --bundles)
+    BUNDLES=true
+    shift
+    ;;
+    --full)
+    FULL=true
+    shift
+    ;;
+esac
+shift
+done
 
 components=("Field" "Option" "Prototype" "Resource")
 bundles=("FieldBundle" "OptionBundle" "PrototypeBundle" "ResourceBundle" "SettingsBundle")
@@ -21,16 +46,18 @@ bundles=("FieldBundle" "OptionBundle" "PrototypeBundle" "ResourceBundle" "Settin
 git subsplit init git@gitlab.med.upenn.edu:dag-framework/Framework.git
 git subsplit update
 
-for component in "${components[@]}"
-do
-    echo "Splitting ${component} component"
-    git subsplit publish "src/Component/${component}:git@gitlab.med.upenn.edu:dag-framework/${component}.git" --heads="master 1.0"
-    sleep 1 # Added for coherence of output
-done
+if [ "$COMPONENTS" = true ]; then
+    for component in "${components[@]}"
+    do
+        echo "Splitting ${component} component"
+        git subsplit publish "src/Component/${component}:git@gitlab.med.upenn.edu:dag-framework/${component}.git" --heads="master 1.0"
+    done
+fi
 
-for bundle in "${bundles[@]}"
-do
-    echo "Splitting ${bundle} bundle"
-    git subsplit publish "src/Bundle/${bundle}:git@gitlab.med.upenn.edu:dag-framework/${bundle}.git" --heads="master 1.0"
-    sleep 1 # Added for coherence of output
-done
+if [ "$BUNDLES" = true ]; then
+    for bundle in "${bundles[@]}"
+    do
+        echo "Splitting ${bundle} bundle"
+        git subsplit publish "src/Bundle/${bundle}:git@gitlab.med.upenn.edu:dag-framework/${bundle}.git" --heads="master 1.0"
+    done
+fi
